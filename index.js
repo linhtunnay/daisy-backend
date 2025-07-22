0c34f9eac163422a467a93697809d8b550aabc3b
linhtunnay <linhtunnay43@gmail.com>
2025-07-22 11:25:01 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,12 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-app.use(express.static("public"));
-
-let userCounter = 1912002;
-const users = {};
-
-io.on("connection", (socket) => {
-  let assignedId = userCounter++;
-  users[socket.id] = assignedId;
-  socket.emit("your-id", assignedId);
-
-  socket.on("send-message", ({ to, message }) => {
-    for (let id in users) {
-      if (users[id] === to) {
-        io.to(id).emit("receive-message", {
-          from: users[socket.id],
-          message
-        });
-        break;
-      }
-    }
-    setTimeout(() => {
-      socket.emit("delete-message");
-    }, 20000); // 20 seconds auto delete
-  });
-
-  socket.on("disconnect", () => {
-    delete users[socket.id];
-  });
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log("Server running on port", port);
-});
+function sendMessage() {
+  const input = document.getElementById("messageInput");
+  const message = input.value.trim();
+  if (message !== "") {
+    const messageElement = document.createElement("div");
+    messageElement.className = "message";
+    messageElement.textContent = message;
+    document.getElementById("messages").appendChild(messageElement);
+    input.value = "";
+    input.focus();
+  }
+}
